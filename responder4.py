5381c5308359daa5f4b863ede49d9a15d6417545
luciana-cyber <61718917+luciana-cyber@users.noreply.github.com>
2020-08-14 14:41:04 -0300

    Add files via upload

---
+++ responder4.py
@@ -0,0 +1,9 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Mariana/AppData/Local/Programs/Python/Python38/responder4.py
+hora:14
+minuto:40
+segundo:32
+ total de segundos:  2446.0
+>>> 
