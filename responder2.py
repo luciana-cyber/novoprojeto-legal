c43754517a8fea45f1962dc09666172ea68d4652
luciana-cyber <61718917+luciana-cyber@users.noreply.github.com>
2020-08-14 14:01:58 -0300

    Add files via upload

---
+++ responder2.py
@@ -0,0 +1,6 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Mariana/AppData/Local/Programs/Python/Python38/responder2.py
+6.0
+>>> 
