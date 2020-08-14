9d3376347d6f60e620fe2524a4b9c7157952cac2
luciana-cyber <61718917+luciana-cyber@users.noreply.github.com>
2020-08-14 14:58:42 -0300

    Add files via upload

---
+++ responder5.py
@@ -0,0 +1,15 @@
+Python 3.8.1 (tags/v3.8.1:1b293b6, Dec 18 2019, 23:11:46) [MSC v.1916 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+= RESTART: C:/Users/Mariana/AppData/Local/Programs/Python/Python38/responder5.py
+valor da altura:38
+Valor do Comprimento:64
+Valor da Largura:236
+38 64 236 15104 573952 22800
+>>> 
+= RESTART: C:/Users/Mariana/AppData/Local/Programs/Python/Python38/responder5.py
+valor da altura:54
+Valor do Comprimento:230
+Valor da Largura:89
+54 230 89 area do piso: 20470 volume da sala: 1105380 area das paredes: 34452
+>>> 
